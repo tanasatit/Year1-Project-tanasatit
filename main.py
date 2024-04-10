7328a9e549f8a9cf9f6655a1dfca05625c8860ce
tanasatit <tanasatit.n@ku.th>
2024-04-10 11:05:55 +0700

    upload csv

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
 
+
+class DemoUI(tk.Tk):
+    def __int__(self):
+        self.title("Playstation")
+
+    def init_components(self):
+        pass
+
+    def run(self):
+        self.mainloop()
+
+
+if __name__ == '__main__':
+    x = DemoUI()
+    x.run()
